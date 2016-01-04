47e43a5a2ba36f7d7f7f3e81e8fe62ed2cde9d7a
Cullen MacDonald <cullenbmacdonald@gmail.com>
2016-01-04 11:04:14 -0500

    because this was removed from the internet

---
+++ setup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='sforce',
+      version='0.0.1',
+      description='Old school api client',
+      author='the internet',
+      packages=['sforce'],
+      install_requires=["requests"],
+)
+
